279e9430ec292198815bf66614e848a95ee84fb9
crcox <cox.crc@gmail.com>
2014-06-05 13:11:01 -0500

    Fixed some error
    I fixed some mistakes I made last time.

--- analysis02_conv.m
+++ analysis02_conv.m
@@ -7,21 +7,23 @@
 % other, while in the second data set (X2), the signals are far away from
 % each other. It is predicted that convolution method will detect the
 % signals form the fist data set more easily.
-
+clear;clc
 
 %% 1. Generating random data sets
 ntrials = 20;
 nvoxels = 40;
-X = randn(ntrials,nvoxels); 
+X = randn(ntrials,nvoxels);
+X1 = X;
+X2 = X;
 [m,n] = size(X);
 
 
 %% 2. Adding some signals
-X1(1:10,1) = X(1:10,1) + 1;   % Adding signals neighbouring to each other
-X1(11:20,2) = X(11:20,2) + 1;
+X1(1:10,1) = X1(1:10,1) + 1;   % Adding signals neighbouring to each other
+X1(11:20,2) = X1(11:20,2) + 1;
 
-X2(1:10,1) = X(1:10,1) + 1;   % Adding signals that far away from each other
-X2(11:20,2) = X(11:20,11) + 1;
+X2(1:10,1) = X2(1:10,1) + 1;   % Adding signals that far away from each other
+X2(11:20,11) = X2(11:20,11) + 1;
 
 
 %% 3. Convolve the signal.
@@ -34,18 +36,18 @@ X2(11:20,2) = X(11:20,11) + 1;
 for i = 1 : m    
     Y1(i,:) = conv(X1(i,:), [.2, .6, .2], 'same');
     
-    if i == m    % disp the result after all rows were convolved
-        disp(Y1)
-    end
+%     if i == m    % disp the result after all rows were convolved
+%         disp(Y1)
+%     end
 end
 
 % Convolve X2
 for i = 1 : m    
     Y2(i,:) = conv(X2(i,:), [.2, .6, .2], 'same');
     
-    if i == m   % disp the result after all rows were convolved
-        disp(Y2)
-    end
+%     if i == m   % disp the result after all rows were convolved
+%         disp(Y2)
+%     end
 end
 
 
@@ -54,4 +56,3 @@ end
 % convolution.
 h1 = ttest(X1(1:10,:), X1(11:20,:))
 h2 = ttest(X2(1:10,:), X2(11:20,:))
-
