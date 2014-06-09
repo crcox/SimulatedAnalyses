0730c2bf8c1bd14861e692f73506510f0e34f7aa
crcox <cox.crc@gmail.com>
2014-06-09 16:23:22 -0500

    Store the indices
    In this version, the indices were stored in two variables, in order to
    make this CV indices generator easy to apply.

--- CV_ordered_withLabels.m
+++ CV_ordered_withLabels.m
@@ -16,7 +16,6 @@ k = 5;
 % It needs to know the number of row labels
 numRL = 2;
 
-
 %% Create the indices matrix
 ind_A = 1:k;
 ind_A = repmat(ind_A', 1, ntrials/k/numRL);
@@ -26,16 +25,20 @@ indices = repmat(indices,numRL,1);
 A = 1:ntrials;
 A = repmat(A', 1, nvoxels);
 
-
 for i = 1:k
 
 ind_test = indices == i;
 ind_train = ~ ind_test;
 
+% Store the indices into a matirx for future use
+indices_test(:,i) = find(ind_test);  
+indices_train(:,i) = find(ind_train); 
+
+% This part can check whether the indices assignment is correct
+% It could be ignored/commented
 ind_test = find(ind_test);
 ind_train = find(ind_train);
-
 disp(['This is the ' num2str(i) 'th CV, the test set indices are: '])
-disp(ind_test')   
+disp(ind_test')  
 
 end
