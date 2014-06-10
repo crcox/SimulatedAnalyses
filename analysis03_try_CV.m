1cb5326a94ef166fb3035ce397b45a9f9e350af4
crcox <cox.crc@gmail.com>
2014-06-10 11:27:12 -0500

    Full Cross Validation
    The second code (analysis04_full_CV_2) is the full cross validation.
    The other code is an intermediate step.
    
    The visualization of the results remains to be done, as there is some
    issues when storing the outputs for the Lasso.

--- analysis03_try_CV.m
+++ analysis03_try_CV.m
@@ -2,17 +2,18 @@
 clear;clc
 
 %% Simulate a data set
-ntrials = 100;
+ntrials = 200;
 nvoxels = 200;
 X = randn(ntrials,nvoxels);   % noise
 size(X);
 
 % Add some signals
-X(1:50,1) = X(1:50,1) + 1;
-X(51:100,2) = X(51:100,2) + 1;
+X(1:ntrials/2,1) = X(1:ntrials/2,1) + 1;
+X(ntrials/2 + 1:end,2) = X(ntrials/2 + 1 : end ,2) + 1;
 % Add row label
-rowLabels = zeros(100,1);
-rowLabels(1:50,1) = 1;
+rowLabels = zeros(ntrials,1);
+rowLabels(1:ntrials /2 ,1) = 1;
+numRL = 2; % number of labels
 
 %% Cross validation
 % Let's do 5 folds CV. In the 1st CV, I will take out trials 1:10 and
@@ -20,34 +21,42 @@ rowLabels(1:50,1) = 1;
 % model on the holdout set. 
 
 %% Subset: 1st CV block
-% training set
-Xtrain1 = X(11:50, :);
-Xtrain2 = X(61:100, :);
-Xtrain = vertcat(Xtrain1,Xtrain2);
+% K-folds
+k = 5;
+block = ntrials /k /numRL;
+
 % test set
-Xtest1 = X(1:10,:);
-Xtest2 = X(51:60,:);
-Xtest = vertcat(Xtest1, Xtest2);
-% new labels
-rowLabelsTrain = zeros(80,1);
-rowLabelsTrain(1:40,1) = 1;
-rowLabelsTest = zeros(20,1);
-rowLabelsTrain(1:10,1) = 1;
+Xtest1 = X(1:block,:);
+Xtest2 = X((ntrials / numRL) +1 : ntrials / numRL + block,:);
+Xtest = vertcat(Xtest1, Xtest2); 
+size_test = size(Xtest);
+
+% training set
+Xtrain1 = X(block + 1: ntrials / numRL, :);
+Xtrain2 = X(ntrials / numRL + block + 1: end, :);
+Xtrain = vertcat(Xtrain1,Xtrain2); 
+
+
+% new labels (2 labels condition)
+rowLabelsTrain = zeros(ntrials - size_test(1),1); 
+rowLabelsTrain(1: (ntrials - size_test(1))/2 ,1) = 1; 
+rowLabelsTest = zeros(size_test(1),1); 
+rowLabelsTest(1:size_test(1) / 2 ,1) = 1; 
 
 
 %% Analysis
-
 % Fit LASSO
 fit = glmnet(Xtrain, rowLabelsTrain, 'binomial')  
 
 % Results on training set, which can be ignored
-(Xtrain * fit.beta) + repmat(fit.a0, 80,1)       % prediction
-((Xtrain * fit.beta) + repmat(fit.a0, 80,1)) > 0 % how well does the prediction fits the truth
+(Xtrain * fit.beta) + repmat(fit.a0, ntrials - size_test(1),1);     
+% how well does the prediction fits the truth
+((Xtrain * fit.beta) + repmat(fit.a0, ntrials - size_test(1),1)) > 0 
 
 fit.df'                                  % How many voxels were selected for each lambda
 fit.lambda                              % lambda values
 
-predic_train = (Xtrain * fit.beta) + repmat(fit.a0, 80,1) > 0   % prediction
+predic_train = (Xtrain * fit.beta) + repmat(fit.a0, ntrials - size_test(1),1) > 0   % prediction
 repmat(rowLabelsTrain,1,100) == predic_train          % compare prediction with truth
 acc_train = mean(repmat(rowLabelsTrain,1,100) == predic_train)'   % accuracy
 
@@ -55,23 +64,27 @@ acc_train = mean(repmat(rowLabelsTrain,1,100) == predic_train)'   % accuracy
 % imagesc(fit.beta)       % two ways of visualize voxel selection process
 
 % Results on testing set
-Xtest*fit.beta + repmat(fit.a0, 20, 1)          % prediction
-predic_test = (Xtest*fit.beta + repmat(fit.a0, 20, 1))> 0     % How well does it fits the truth
+Xtest*fit.beta + repmat(fit.a0, size_test(1), 1)          % prediction
+predic_test = (Xtest*fit.beta + repmat(fit.a0, size_test(1), 1))> 0     % How well does it fits the truth
 predic_test == repmat(rowLabelsTest,1,100)
 acc_test = mean(repmat(rowLabelsTest,1,100) == predic_test)' 
 
 max_acc = max(acc_test)
+max_ind = find(acc_test == max(acc_test)); % store the indices for the best lambdas
 
-%% Visualizing result
+
+%% Visualizing the results
 plot(acc_train, 'Linewidth', 2)
 hold on 
 plot(acc_test, 'r', 'Linewidth', 2) 
 max_xrange = 0: 0.5: 100;
 plot(max_xrange,max_acc, 'g', 'Linewidth', 2)
+chance_range = 0: 0.5 : 100;
+plot(chance_range, 0.5,'k', 'linewidth', 2)
 hold off
 
-
+axis([1 100 0 1])
 legend('accuary train', 'accuracy test','max for accuracy test',...
     'Location','SouthEast')
 xlabel('Trails')
-ylabel('Accuracy')
+ylabel('Accuracy (chance = 0.5)')
