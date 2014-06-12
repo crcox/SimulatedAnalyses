3220acbf42881304bc8230e0537d3ada46e8f940
crcox <cox.crc@gmail.com>
2014-06-12 14:35:15 -0500

    Lasso with full CV
    It is almost finished. I still need to add some code to find which
    voxels were consistently used.

--- analysis04_full_CV_2.m
+++ analysis04_full_CV_2.m
@@ -4,10 +4,11 @@
 
 %% WARNING: This will clear the work space & variables.
 clear;clc;
+rng(1)  % To make the result replicable
 
 %% You can set the parameters for CV here
 % Please the dimension of the data sets
-ntrials = 200;
+ntrials = 400;
 nvoxels = 200;
 % Please set the number of folds 
 k = 5;
@@ -21,7 +22,7 @@ size_test = ntrials / k ;
 % Add some noise
 X = randn(ntrials,nvoxels);   
 size(X);
-% Add some signals (suppose there are 2 row labels )
+% Add some signals 
 X(1:ntrials/numRL,1) = X(1:ntrials/numRL,1) + 1;
 X(ntrials/numRL + 1:end,2) = X(ntrials/numRL + 1 : end ,2) + 1;
 
@@ -56,64 +57,81 @@ for i = 1:k
     indices_test(:,i) = find(ind_test);  
     indices_train(:,i) = find(ind_train); 
 
-    % This part can check whether the indices assignment is correct
-%     ind_test = find(ind_test);
-%     ind_train = find(ind_train);
-%     disp(['This is the ' num2str(i) 'th CV, the test set indices are: '])
-%     disp(ind_test')  
-
 end
 
 
 %% Analysis
+
+% Initialize some variable to store results
+
+acc_test = NaN(100, k);
+acc_train = NaN(100, k);
+
+
+% Start loop. For each CV, do a lasso.
 for j = 1:k
-% Subset the data set
-Xtest = X(indices_test(:,j) ,:);
-Xtrain = X(indices_train(:,j) ,:);
+    % Subset the data set
+    Xtest = X(indices_test(:,j) ,:);
+    Xtrain = X(indices_train(:,j) ,:);
 
-% Fit LASSO
-fit = glmnet(Xtrain, rowLabelsTrain, 'binomial');  
-dim_lambda = size(fit.lambda);
-num_lambda = dim_lambda(1);
+    % Fit LASSO
+    fit = glmnet(Xtrain, rowLabelsTrain, 'binomial');  
 
-% Results on training set, which can be ignored
-(Xtrain * fit.beta) + repmat(fit.a0, ntrials - size_test,1);   
-predic_train = (Xtrain * fit.beta) + repmat(fit.a0, ntrials - size_test(1),1) > 0;   % prediction
-repmat(rowLabelsTrain,1,num_lambda) == predic_train;          % compare prediction with truth
-acc_train = mean(repmat(rowLabelsTrain,1,num_lambda) == predic_train)';   % accuracy
+    % Get the number of lambda
+    dim_lambda = size(fit.lambda);
+    num_lambda = dim_lambda(1);
+    
+    % Results on training set, which can be ignored 
+    predic_train = (Xtrain * fit.beta) + repmat(fit.a0, ntrials - size_test,1) > 0;     % Prediction
+    acc_train(:,j) = mean(repmat(rowLabelsTrain,1,num_lambda) == predic_train)';             % Accuracy
 
-% Results on testing set
-Xtest*fit.beta + repmat(fit.a0, size_test(1), 1);          % prediction
-predic_test = (Xtest*fit.beta + repmat(fit.a0, size_test(1), 1))> 0 ;    % How well does it fits the truth
-predic_test == repmat(rowLabelsTest,1,num_lambda);
-acc_test = mean(repmat(rowLabelsTest,1,num_lambda) == predic_test)' ;
+    % Results on testing set
+    predic_test = (Xtest*fit.beta + repmat(fit.a0, size_test, 1))> 0 ;    % Predictiion
+    acc_test(:,j) = mean(repmat(rowLabelsTest,1,num_lambda) == predic_test)';  % Accuracy
 
-max_acc(j) = max(acc_test);
-% max_ind = find(acc_test == max(acc_test)); % store the indices for the best lambdas
+
 end
 
-% show the results
-disp(['The maximun accuracy for the best lambda in each CV are:'])
-disp(max_acc)
 
-% %% Visualizing the results
-% % Plot the accracy for the training set, just to compare
-% plot(acc_train, 'Linewidth', 2)
-% hold on 
-% % Plot the accuracy for the testing set
-% plot(acc_test, 'r', 'Linewidth', 2) 
-% max_xrange = 0: 0.5: 100;
-% % Plot the maximun classification accuracy
-% plot(max_xrange,max_acc, 'g', 'Linewidth', 2)
-% % Plot the chance line
-% chance_range = 0: 0.5 : 100;
-% plot(chance_range, 0.5,'k', 'linewidth', 2)
-% hold off
-% 
-% axis([1 num_lambda 0 1])
-% legend('accuary train', 'accuracy test','max for accuracy test',...
+% Find maximun accuracy for each CV
+max_acc = max(acc_test);
+disp(['The maximun accuracy(with the best lambda) in each CV are:'])
+disp(max_acc')
+
+
+% For each lambda, find average accuracy
+mean_acc_train = mean(acc_train,2); % training set
+mean_acc_test = mean(acc_test,2);   % testing set
+% Find the maximum accuracy after taking average
+max_mean_acc = max(mean_acc_test);
+disp(['Average accuracies for each lambda were computed. The maximun accuracy is '])
+disp(max_mean_acc)
+
+% Find the indices for the maximun accuracy
+find(mean_acc_test == max(mean_acc_test));
+% Display how many voxels were used for the best performance
+disp(['How many voxels did the Lasso used, to produce the max_accuracy?'])
+disp(fit.df(find (mean_acc_test == max(mean_acc_test))))
+
+
+%% Visualizing the results
+% Plot the accracy for the training set, just to compare
+plot(mean_acc_train, 'Linewidth', 2)
+hold on 
+% Plot the accuracy for the testing set
+plot(mean_acc_test, 'r', 'Linewidth', 2) 
+max_xrange = 0: 0.5: 100;
+% Plot the maximun classification accuracy
+plot(max_xrange,max_mean_acc, 'g', 'Linewidth', 2)
+% Plot the chance line
+chance_range = 0: 0.5 : 100;
+plot(chance_range, 0.5,'k', 'linewidth', 2)
+hold off
+
+axis([1 num_lambda 0 1])
+legend('accuary train', 'accuracy test','max - accuracy test',...
+    'Location','SouthEast')
+% legend( 'accuracy test','max for accuracy test',...
 %     'Location','SouthEast')
-% % legend( 'accuracy test','max for accuracy test',...
-% %     'Location','SouthEast')
-% xlabel('Trails')
-% ylabel('Accuracy (chance = 0.5)')
+xlabel('Trails')
+ylabel('Accuracy (chance = 0.5)')
