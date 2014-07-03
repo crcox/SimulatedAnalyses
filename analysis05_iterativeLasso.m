b38c1ecf6bb7feb01caa937b2426f7a13e0338f5
crcox <cox.crc@gmail.com>
2014-07-03 13:42:39 -0500

    Iterative Lasso (manual)
    It can run iterative lasso manually.

--- analysis05_iterativeLasso.m
+++ analysis05_iterativeLasso.m
@@ -5,45 +5,46 @@ clear;clc;
 
 %% You can set the parameters for CV here
 % Please the dimension of the data sets 
-ntrials = 100; % it has to be divisible by K
-nvoxels = 100;
+ntrials = 150; % it has to be divisible by K
+nvoxels = 150;
 % Please set the number of folds 
 k = 5;
 % It needs to know the number of row labels
 % ps:Currently, this program can just run normally when there are 2 rowlabels
 rowLabels.num = 2;
+
+% Set the strength of signal and noise
+signal = 1;
+numsignal = 10;
+rng(1) % To make the result replicable
+noise = 1.5;
+
 % It is useful to know the size for the testing set
 test.size = ntrials / k ;
 
 % Display the parameters
 disp ('Parameters: ')
-disp(['Voxels: '  num2str(nvoxels)])
-disp(['trials: '  num2str(ntrials)])
-disp(['K : '  num2str(k)])
-disp(['Number of row labels: ' num2str(rowLabels.num)])
-
-
+disp(['number of Voxels= '  num2str(nvoxels)])
+disp(['number of Trials= '  num2str(ntrials)])
+disp(['K = '  num2str(k) ' (for K-folds CV)' ])
+disp(['Number of row labels= ' num2str(rowLabels.num)])
+disp(['Signal intensity = ' num2str(signal)])
+disp(['Noise intensity = ' num2str(noise)])
 
 %% Simulate the data
 % Creating the background 
 X.raw = zeros(ntrials, nvoxels);
 
 % % Add some signals 
-signal = 1;
-disp(['Signal intensity = ' num2str(signal)])
-X.raw(1:ntrials/rowLabels.num,1:15) = X.raw(1:ntrials/rowLabels.num,1:15) + signal;
-X.raw(ntrials/rowLabels.num + 1:end,16:30) = X.raw(ntrials/rowLabels.num + 1 : end ,16:30) + signal;
+X.raw(1:ntrials/rowLabels.num,1:numsignal / 2) = X.raw(1:ntrials/rowLabels.num,1:numsignal/2) + signal;
+X.raw(ntrials/rowLabels.num + 1:end, numsignal/2 + 1 : numsignal) = X.raw(ntrials/rowLabels.num + 1 : end ,numsignal/2 + 1 : numsignal) + signal;
 % plot the signal
 figure(1)
 imagesc(X.raw)
 xlabel('Voxels');ylabel('Trials');title('Signal');
 
 % Adding noise 
-rng(1) % To make the result replicable
-noise = 1;
-disp(['Noise intensity = ' num2str(noise)])
 X.raw = X.raw + noise * randn(ntrials,nvoxels);   
-size(X.raw);
 % plot the noise + signal
 figure(2)
 imagesc(X.raw)
@@ -52,6 +53,8 @@ xlabel('Voxels');ylabel('Trials');title('Signal & Noise')
 % Create row labels
 rowLabels.whole = zeros(ntrials,1);
 rowLabels.whole(1:ntrials / rowLabels.num ,1) = 1;
+% imagesc(rowLabels.whole);
+
 % Create new row labels for CV blocks
 rowLabels.train = zeros(ntrials - test.size,1); 
 rowLabels.train(1: (ntrials - test.size)/rowLabels.num ,1) = 1; 
@@ -65,19 +68,18 @@ rowLabels.test(1:test.size / rowLabels.num ,1) = 1;
 CV.blocks = 1:k;
 CV.blocks = CV.blocks';
 CV.indices = repmat(CV.blocks,[ntrials / k,1]);
-<<<<<<< HEAD
+
 % Generate indices for CVglmnet
 CV2.blocks = 1:k-1;
 CV2.blocks = CV2.blocks';
 CV2.indices = repmat(CV2.blocks,[ntrials / k,1]);
-=======
+
 % Generate indices for CV (after the test set was taken out)
 CV2.blocks = 1 : k-1;
 CV2.blocks = CV2.blocks';
 CV2.indices = repmat(CV2.blocks,[ntrials / k,1]);
 
 
->>>>>>> FETCH_HEAD
 % Creating indices for test set and training set
 for i = 1: k
     test.indices(:,i) = CV.indices == i;
@@ -87,6 +89,7 @@ end
 
 
 %% Iterative Lasso
+
 % Try the 1st iteration
 for i = 1:k
     % Split the data into training set and testing set 
@@ -94,15 +97,12 @@ for i = 1:k
     X.train = X.raw(train.indices(:,i) ,:);
 
     % Fit cvglmnet
-<<<<<<< HEAD
     cvfit(i) = cvglmnet (X.train, rowLabels.train, 'binomial', 'class', CV2.indices', 4);
-=======
-    cvfit(i) = cvglmnet (X.train, rowLabels.train, 'binomial', 'class', CV2.indices',4);
->>>>>>> FETCH_HEAD
+
     % Get the indice for the lambda with the best accuracy 
     lambda.best(i) = find(cvfit(i).lambda == cvfit(i).lambda_min);
     % Plot the cross-validation curve
-    cvglmnetPlot(cvfit(i));
+%     cvglmnetPlot(cvfit(i));
 
     % Set the lambda value
     opts(i) = glmnetSet();
@@ -117,12 +117,14 @@ for i = 1:k
     
     % Find indices for the voxels that have been used
     voxel(i).used = find (fit(i).beta ~= 0);
-    % Find true signals that have been identified
-    voxel(i).signal = sum(voxel(i).used <= 10);
     % Find indices for the voxels that have not been used
-    voxel(i).remain = find (fit(i).beta == 0);
-    % How many voxels have been used for each iteration
-    voxel(i).num = sum(fit(i).beta ~= 0);
+    voxel(i).remain = find (fit(i).beta == 0);    
+    
+    % How many voxels have been used for each iteration?
+    voxel(i).num = sum(fit(i).beta ~= 0);    
+    % How many of them are true signals?
+    voxel(i).signal = sum(voxel(i).used <= numsignal);
+
 
 end
 
@@ -164,19 +166,24 @@ for i = 1:k
     % Evaluate the prediction 
     test.prediction(:,i) = (X.test * fit(i).beta + repmat(fit(i).a0, [test.size, 1])) > 0 ;  
     test.accuracy(:,i) = mean(rowLabels.test == test.prediction(:,i))'
-    
-    % Find indices for the voxels that have been used
-    voxel(i).used = find (fit(i).beta ~= 0);
-    % Find true signals that have been identified
-    voxel(i).signal = sum(voxel(i).used <= 10);
-    % Find indices for the voxels that have not been used
-    voxel(i).remain = find (fit(i).beta == 0);
-    % How many voxels have been used for each iteration
-    voxel(i).num = sum(fit(i).beta ~= 0);
-    
+%     
+%     % Find indices for the voxels that have been used
+%     voxel(i).used = find (fit(i).beta ~= 0);
+%     % Find true signals that have been identified
+%     voxel(i).signal = sum(voxel(i).used <= numsignal);
+%     % Find indices for the voxels that have not been used
+%     voxel(i).remain = find (fit(i).beta == 0);
+%     % How many voxels have been used for each iteration
+%     voxel(i).num = sum(fit(i).beta ~= 0);
+%     
 end
 
 % Display the average accuracy for this procedure 
 disp(['The mean accuracy is ' num2str(mean(test.accuracy))])
 % See if it is better than chance 
 ttest(test.accuracy, 0.5)
+
+disp('number of voxels')
+voxel.num
+disp('number of signals')
+voxel.signal
