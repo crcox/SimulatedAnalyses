1ce8d779bd2951da49942d0452b07acb6d59f22b
crcox <cox.crc@gmail.com>
2014-07-08 10:14:35 -0500

    Function for simulating data
    I turned the 1st part of the simulation to a function

--- simData.m
+++ simData.m
@@ -1,25 +1,37 @@
-function [ output ] = simData( numtrials, numvoxels, numfolds, numRowLabels, signalStrength, noiseStrength )
+function [ ntrials, nvoxels, k, signal, noise, rowLabels,numsignal, testSize, X ] =...
+    simData( ntrials, nvoxels, k, signal, noise, numRowLabels, numsignal )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 %% You can set the parameters for CV here
 % Please the dimension of the data sets 
-ntrials = numtrials; % it has to be divisible by K
-nvoxels = numvoxels;
+% ntrials = 150; % it has to be divisible by K
+% nvoxels = 150;
 % Please set the number of folds 
-k = numfolds;
+% k = 5;
 % It needs to know the number of row labels
 % ps:Currently, this program can just run normally when there are 2 rowlabels
 rowLabels.num = 2;
-% It is useful to know the size for the testing set
 
-test.size = ntrials / k; 
+% Set the strength of the signal 
+% signal = 1;
+% numsignal = 20;
+% Set the strength of the noise
+ 
+% noise = 1.5;
+
+% It is useful to know the size for the testing set
+testSize = ntrials / k ;
 
 % Display the parameters
 disp ('Parameters: ')
-disp(['Voxels: '  num2str(nvoxels)])
-disp(['trials: '  num2str(ntrials)])
-disp(['K : '  num2str(k)])
-disp(['Number of row labels: ' num2str(rowLabels.num)])
+disp(['number of Voxels= '  num2str(nvoxels)])
+disp(['number of Trials= '  num2str(ntrials)])
+disp(['K = '  num2str(k) ' (for K-folds CV)' ])
+disp(['Number of row labels= ' num2str(rowLabels.num)])
+disp(['Signal intensity = ' num2str(signal)])
+disp(['Noise intensity = ' num2str(noise)])
+disp(['Number of signals = ' num2str(numsignal)])
+
 
 
 %% Simulate the data
@@ -27,21 +39,15 @@ disp(['Number of row labels: ' num2str(rowLabels.num)])
 X.raw = zeros(ntrials, nvoxels);
 
 % % Add some signals 
-signal = signalStrength;
-disp(['Signal intensity = ' num2str(signal)])
-X.raw(1:ntrials/rowLabels.num,1:5) = X.raw(1:ntrials/rowLabels.num,1:5) + signal;
-X.raw(ntrials/rowLabels.num + 1:end,6:10) = X.raw(ntrials/rowLabels.num + 1 : end ,6:10) + signal;
+X.raw(1:ntrials/rowLabels.num,1:numsignal / 2) = X.raw(1:ntrials/rowLabels.num,1:numsignal/2) + signal;
+X.raw(ntrials/rowLabels.num + 1:end, numsignal/2 + 1 : numsignal) = X.raw(ntrials/rowLabels.num + 1 : end ,numsignal/2 + 1 : numsignal) + signal;
 % plot the signal
 figure(1)
 imagesc(X.raw)
 xlabel('Voxels');ylabel('Trials');title('Signal');
 
 % Adding noise 
-% rng(1) % To make the result replicable
-noise = noiseStrength;
-disp(['Noise intensity = ' num2str(noise)])
 X.raw = X.raw + noise * randn(ntrials,nvoxels);   
-size(X.raw);
 % plot the noise + signal
 figure(2)
 imagesc(X.raw)
@@ -50,11 +56,13 @@ xlabel('Voxels');ylabel('Trials');title('Signal & Noise')
 % Create row labels
 rowLabels.whole = zeros(ntrials,1);
 rowLabels.whole(1:ntrials / rowLabels.num ,1) = 1;
+% imagesc(rowLabels.whole);
+
 % Create new row labels for CV blocks
-rowLabels.train = zeros(ntrials - test.size,1); 
-rowLabels.train(1: (ntrials - test.size)/rowLabels.num ,1) = 1; 
-rowLabels.test = zeros(test.size,1); 
-rowLabels.test(1:test.size / rowLabels.num ,1) = 1; 
+rowLabels.train = zeros(ntrials - testSize,1); 
+rowLabels.train(1: (ntrials - testSize)/rowLabels.num ,1) = 1; 
+rowLabels.test = zeros(testSize,1); 
+rowLabels.test(1:testSize / rowLabels.num ,1) = 1; 
 
 
 end
