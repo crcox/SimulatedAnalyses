ec2761e628c30fd568b2feb4837da8731451ef95
crcox <cox.crc@gmail.com>
2014-07-11 14:23:20 -0500

    function for cv indices

--- CVindices.m
+++ CVindices.m
@@ -1,4 +1,4 @@
-function [ CV, CV2, test, train ] = CVindices( k, ntrials )
+function [ CV, CV2 ] = CVindices( k, ntrials )
 %UNTITLED11 Summary of this function goes here
 %   Detailed explanation goes here
 %% Generating indices for Outer CV
@@ -16,13 +16,6 @@ CV2.indices = repmat(CV2.blocks,[ntrials / k,1]);
 CV2.blocks = 1 : k-1;
 CV2.blocks = CV2.blocks';
 CV2.indices = repmat(CV2.blocks,[ntrials / k,1]);
-
-
-% Creating indices for test set and training set
-for i = 1: k
-    test.indices(:,i) = CV.indices == i;
-    train.indices(:,i) = CV.indices ~= i;
-end
 
 end
 
