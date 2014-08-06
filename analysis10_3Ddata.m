a8dfaf9a6c7d9648b21385cee2b44b6c47ac2ecf
QihongL <lvqihong1992@gmail.com>
2014-08-06 16:45:31 -0500

    Simulate 3D data

--- analysis10_3Ddata.m
+++ analysis10_3Ddata.m
@@ -1,10 +1,11 @@
 %% Simulate 3D data
 clear; clc; close all;
 
+%% Create 3d coordinates 
 
 % Set some parameters
 length = 10;
-numTrials = 100;
+numTrials = 300;
 numVoxels = length ^ 3;
 
 % Assign length for x y z 
@@ -26,23 +27,31 @@ map = [tempx, tempy, tempz];
 figure(1)
 scatter3(tempx,tempy,tempz);
 
-% Create
+% Create the data set 
 X = zeros(numVoxels, numTrials);
 
 
-%% Add signal to a cluster
+
+%% Adding signal to a cluster
 % Set the range of the cluster
-upper = 4;
+upper = 5;
 lower = 2;
 
 % Get coordinates that satisfy the range
 for i = 1 : numVoxels
     if sum(map(i,:) <= upper) == 3 && sum(map(i,:) >= lower) == 3
         tempCod(i,:) = map(i,:);
+    else
+        tempCod(i,1:3) = NaN;
     end
 end
+
+% Get indices for voxels in the range 
+signalInd = find(~isnan(tempCod(:,1)));
+
 % Get rid of zero row
 tempCod( ~any(tempCod,2), : ) = [];
+
 % Transform to a form that allows scatter3()
 sigx = tempCod(:,1);
 sigy = tempCod(:,2);
@@ -50,11 +59,13 @@ sigz = tempCod(:,3);
 
 % Visualize it, make sure that I'm doing the right thing
 figure(1)
-
 hold on 
 scatter3(tempx,tempy,tempz, 'b');
 scatter3(sigx,sigy,sigz, 'r');
 hold off
+
+
+%% Adding noise 
 
 
 
