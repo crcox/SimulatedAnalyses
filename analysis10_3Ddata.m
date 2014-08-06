af30284ef095a3e64a426a9d6b57e01f92e6d7f0
QihongL <lvqihong1992@gmail.com>
2014-08-06 15:43:32 -0500

    Simulate 3d data

--- analysis10_3Ddata.m
+++ analysis10_3Ddata.m
@@ -19,15 +19,42 @@ tempy = repmat(1:x, z);
 tempy = tempy(:);
 tempz = sort(tempx);
 
-% Concatenation
+% Concatenation, get the coordinates  
 map = [tempx, tempy, tempz];
-size(map);
 
 % check if I am doing the right thing
+figure(1)
 scatter3(tempx,tempy,tempz);
-
 
 % Create
 X = zeros(numVoxels, numTrials);
+
+
+%% Add signal to a cluster
+% Set the range of the cluster
+upper = 4;
+lower = 2;
+
+% Get coordinates that satisfy the range
+for i = 1 : numVoxels
+    if sum(map(i,:) <= upper) == 3 && sum(map(i,:) >= lower) == 3
+        tempCod(i,:) = map(i,:);
+    end
+end
+% Get rid of zero row
+tempCod( ~any(tempCod,2), : ) = [];
+% Transform to a form that allows scatter3()
+sigx = tempCod(:,1);
+sigy = tempCod(:,2);
+sigz = tempCod(:,3);
+
+% Visualize it, make sure that I'm doing the right thing
+figure(1)
+
+hold on 
+scatter3(tempx,tempy,tempz, 'b');
+scatter3(sigx,sigy,sigz, 'r');
+hold off
+
 
 
